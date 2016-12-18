195e74b78d47bac8298a02fc2a783334de64e320
GT3CH1 <gavin.pease@gmail.com>
2016-12-18 10:50:37 -0700

    Did stuff

--- toggle.sh
+++ toggle.sh
@@ -1,9 +1,11 @@
-#!/usr/bin/env bash
-name=$("cat data/1.dat")
-pwd
-if [[ $name == "1" ]]; then
+#!/bin/bash
+name=$(cat data/1.dat)
+echo $name
+if [[ "$name" == "1" ]]; then
   sudo python lib/on.py
+  echo 'turning on tree 1'
 fi
-if [[ $name == "0" ]]; then
+if [[ "$name" == "0" ]]; then
   sudo python lib/off.py
+  echo 'turning off tree 1'
 fi
