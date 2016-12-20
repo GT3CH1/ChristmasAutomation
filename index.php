c8cda63dd205ac267d2081c411963d1b7abd347d
GT3CH1 <gavin.pease@gmail.com>
2016-12-20 08:51:47 -0700

    Working on GUI

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
   //insert logic here
-  $toggle = exec("gpio read 7", $status);
+exec("gpio read 7", $status);
 
-  if($toggle == "1"){
+  if($status[0] == "1"){
     echo 'Tree is on';
   }else{
     echo 'Tree is off';
